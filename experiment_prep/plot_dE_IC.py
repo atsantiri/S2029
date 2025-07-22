74cd3db579d3dc4588cf26325d49950536e4f7c3
atsantiri <artemis_tsantiri@hotmail.com>
2025-07-22 12:36:37 -0600

    added experiment prep LISE files

--- experiment_prep/plot_dE_IC.py
+++ experiment_prep/plot_dE_IC.py
@@ -7,29 +7,38 @@ import numpy as np
 
 beams1 = ['17F', '17O']
 beams2 = ['20Na', '20Mg']
-
+c = ['b','g','magenta']
+lnst=['solid','dashed']
 fig, axs = plt.subplots(1,2, figsize=(10, 6))
+pressure = ['60','70','80']
 
-for b in beams1:
-    with open(f'LISEpp_outputs/{b}_70mbar_IC.txt') as f:
-        clean_lines = (line.replace('-', '0') for line in f)
-        ene, y = np.loadtxt(clean_lines, skiprows=4).T
-    axs[0].plot(ene, y, label=b)
+for j,b in enumerate(beams1):
+    for i,p in enumerate(pressure):
+        with open(f'LISEpp_outputs/{b}_{p}mbar_IC.txt') as f:
+            clean_lines = (line.replace('-', '0') for line in f)
+            ene, y = np.loadtxt(clean_lines, skiprows=4).T
+        axs[0].plot(ene, y, label=f'{b}@{p}mbar',color=c[i],linestyle=lnst[j])
 
-axs[0].legend()
+axs[0].legend(loc='upper left')
+axs[0].set_xlabel('Energy (MeV/u)')
 axs[0].set_ylabel('Counts')
-axs[0].set_title('For 4.5 MeV/u in 70mbar IC')
+axs[0].set_ylim(0,4.7e15)
+axs[0].set_title('For 4.5 MeV/u in IC')
 
-for b in beams2:
-    with open(f'LISEpp_outputs/{b}_70mbar_IC.txt') as f:
-        clean_lines = (line.replace('-', '0') for line in f)
-        ene, y = np.loadtxt(clean_lines, skiprows=4).T
-    axs[1].plot(ene, y, label=b)
+for j,b in enumerate(beams2):
+    for i,p in enumerate(pressure):
+        with open(f'LISEpp_outputs/{b}_{p}mbar_IC.txt') as f:
+            clean_lines = (line.replace('-', '0') for line in f)
+            ene, y = np.loadtxt(clean_lines, skiprows=4).T
+        axs[1].plot(ene, y, label=f'{b}@{p}mbar',color=c[i],linestyle=lnst[j])
 
-axs[1].legend()
+axs[1].legend(loc='upper left')
 axs[1].set_xlabel('Energy (MeV/u)')
 axs[1].set_ylabel('Counts')
-axs[1].set_title('For 6 MeV/u in 70 mbar IC')
+axs[1].set_ylim(0,2.5e15)
+
+axs[1].set_title('For 6 MeV/u in IC')
 
 plt.tight_layout()
+plt.savefig('dE_IC_vary_pressure.png')
 plt.show()
