63a9baddede0640636efa5b1e28231cf3a500462
atsantiri <artemis_tsantiri@hotmail.com>
2025-07-29 08:09:38 -0600

    added CFA to simulation

--- Simulation/Simulation_S2029.cpp
+++ Simulation/Simulation_S2029.cpp
@@ -36,11 +36,39 @@ double dEthroughIC(const ActPhysics::Particle& particle, double energy, bool app
     }
     
     // 11.5 um mylar
-    auto eneAfterICmylar {srim->SlowWithStraggling(tableKey1.Data(), energy, 11.5e-3)}; // 11.5 um mylar in IC 
+    auto eneAfterMylar {srim->SlowWithStraggling(tableKey1.Data(), energy, 11.5e-3)}; // 11.5 um mylar in IC 
+    // auto eneAfterMylar {srim->Slow(tableKey1.Data(), energy, 11.5e-3)}; // 11.5 um mylar in IC 
     // 36 mm CF4 gas
-    auto eneAfterIC {srim->SlowWithStraggling(tableKey2.Data(), eneAfterICmylar, 36)};
+    auto eneAfterIC {srim->SlowWithStraggling(tableKey2.Data(), eneAfterMylar, 36)};
+    // auto eneAfterIC {srim->Slow(tableKey2.Data(), eneAfterMylar, 36)};
     
     return eneAfterIC;
+}
+
+double dEthroughCFA(const ActPhysics::Particle& particle, double energy, bool applyStraggling, ActPhysics::SRIM* srim) {
+    if (!applyStraggling) return energy;
+
+    // Choose the right SRIM table key based on particle name
+    TString tableKey1, tableKey2;
+    if (particle.GetName() == "17F" || particle.GetName() == "20Mg") {
+        tableKey1 = "beamInMylar";
+        tableKey2 = "beamIniC4H10";
+    } else if (particle.GetName() == "17O" || particle.GetName() == "20Na") {
+        tableKey1 = "ContaminantInMylar";
+        tableKey2 = "ContaminantIniC4H10";
+    } else {
+        std::cerr << "Unknown particle in dEthroughIC: " << particle.GetName() << '\n';
+        return energy; 
+    }
+    
+    // 4.8 um mylar
+    auto eneAfterMylar {srim->SlowWithStraggling(tableKey1.Data(), energy, 4.8e-3)};  
+    // auto eneAfterMylar {srim->Slow(tableKey1.Data(), energy, 4.8e-3)}; 
+    // 9.6 mm iC4H10 (isobutane) gas
+    auto eneAfterCFA {srim->SlowWithStraggling(tableKey2.Data(), eneAfterMylar, 9.6)};
+    // auto eneAfterCFA {srim->Slow(tableKey2.Data(), eneAfterMylar, 9.6)};
+    
+    return eneAfterCFA;
 }
 
 void Simulation_S2029(const std::string& beam = "17F", double T1 = 4.5, double Ex = 0){
@@ -69,7 +97,7 @@ void Simulation_S2029(const std::string& beam = "17F", double T1 = 4.5, double E
 
     // Resolutions
     const double sigmaSil {0.060 / 2.355};
-    const double sigmaPercentBeam {0.004};
+    const double sigmaPercentBeam {0.002};
     const double sigmaAngleLight {0.95 / 2.355};
 
     // Kinematics
@@ -85,37 +113,33 @@ void Simulation_S2029(const std::string& beam = "17F", double T1 = 4.5, double E
     //     p1, p2, p3, p4, (protonPS > 0 ? protonPS : 0), (neutronPS > 0 ? neutronPS : (pdphase || dtphase ? 1 : 0))};
     // kingen.Print();
 
-    // Load SRIM Tables
-    auto* srim {new ActPhysics::SRIM()};
-    TString srim_IC_mylar = TString::Format("./SRIM/%s_mylar.txt", beam.c_str());
-    if (std::ifstream(srim_IC_mylar.Data())) {
-        srim->ReadTable("beamInMylar", srim_IC_mylar.Data());
-    } else {
-        std::cerr << srim_IC_mylar <<" not found!\n";
-        return;
-    }
-    TString srim_IC_gas = TString::Format("./SRIM/%s_CF4_%0.fmbar.txt", beam.c_str(), pressureIC);
-    if (std::ifstream(srim_IC_gas.Data())) {
-        srim->ReadTable("beamInCF4", srim_IC_gas.Data());
-    } else {
-        std::cerr << srim_IC_gas <<" not found!\n";
-        return;
-    }
-    TString srim_p1c_mylar = TString::Format("./SRIM/%s_mylar.txt", p1c.GetName().c_str());
-    if (std::ifstream(srim_p1c_mylar.Data())) {
-        srim->ReadTable("ContaminantInMylar", srim_p1c_mylar.Data());
-    } else {
-        std::cerr << srim_p1c_mylar <<" not found!\n";
-        return;
-    }
-    TString srim_p1c_IC_gas = TString::Format("./SRIM/17O_CF4_%0.fmbar.txt", pressureIC);
-    if (std::ifstream(srim_p1c_IC_gas.Data())) {
-        srim->ReadTable("ContaminantInCF4", srim_p1c_IC_gas.Data());
-    } else {
-        std::cerr << srim_p1c_IC_gas <<" not found!\n";
-        return;
-    }    
 
+    // // Load SRIM Tables
+    auto* srim {new ActPhysics::SRIM()};
+
+    auto load_table = [&](const char* label, const TString& path) {
+        if (std::ifstream(path.Data())) {
+            srim->ReadTable(label, path.Data());
+        } else {
+            std::cerr << path << " not found!\n";
+            return false;
+        }
+        return true;
+    };
+
+    std::vector<std::pair<const char*, TString>> tables = {
+        {"beamInMylar", TString::Format("./SRIM/%s_mylar.txt", beam.c_str())},
+        {"beamInCF4", TString::Format("./SRIM/%s_CF4_%.0fmbar.txt", beam.c_str(), pressureIC)},
+        {"beamIniC4H10", TString::Format("./SRIM/%s_iC4H10_%.0fmbar.txt", beam.c_str(), pressureCFA)},
+        {"ContaminantInMylar", TString::Format("./SRIM/%s_mylar.txt", p1c.GetName().c_str())},
+        {"ContaminantInCF4", TString::Format("./SRIM/%s_CF4_%.0fmbar.txt", p1c.GetName().c_str(), pressureIC)},
+        {"ContaminantIniC4H10", TString::Format("./SRIM/%s_iC4H10_%.0fmbar.txt", p1c.GetName().c_str(), pressureCFA)}
+    };
+
+    for (const auto& [label, path] : tables) {
+        if (!load_table(label, path))
+            return;
+    }
 
     //---- SIMULATION STARTS HERE
     ROOT::EnableImplicitMT();
@@ -137,6 +161,8 @@ void Simulation_S2029(const std::string& beam = "17F", double T1 = 4.5, double E
         auto hTCInitial {HistConfig::dE.GetHistogram()};
         auto hT1AfterIC {HistConfig::dE.GetHistogram()};
         auto hTCAfterIC {HistConfig::dE.GetHistogram()};
+        auto hT1AfterCFA {HistConfig::dE.GetHistogram()};
+        auto hTCAfterCFA {HistConfig::dE.GetHistogram()};
 
     for(long int reaction = 0; reaction < iterations; reaction++)
     {
@@ -160,8 +186,11 @@ void Simulation_S2029(const std::string& beam = "17F", double T1 = 4.5, double E
         
         // Pass beam through IC
         auto T1AfterIC {dEthroughIC(p1, T1Initial, stragglingInIC, srim)};
-
         hT1AfterIC->Fill(T1AfterIC/p1.GetAMU());
+
+        // Pass beam through CFA
+        auto T1AfterCFA {dEthroughCFA(p1, T1AfterIC, stragglingInCFA, srim)};
+        hT1AfterCFA->Fill(T1AfterCFA/p1.GetAMU());
 
         // If we want to track the contaminant 17O
         if (trackContamination){
@@ -174,6 +203,8 @@ void Simulation_S2029(const std::string& beam = "17F", double T1 = 4.5, double E
             auto TCAfterIC {dEthroughIC(p1c, TCInitial, stragglingInIC, srim)};
             hTCAfterIC->Fill(TCAfterIC/p1c.GetAMU());
 
+            auto TCAfterCFA {dEthroughCFA(p1c, TCAfterIC, stragglingInCFA, srim)};
+            hTCAfterCFA->Fill(TCAfterCFA/p1c.GetAMU());
         }
 
         // std::cout << "T1AfterIC: " << T1AfterIC/p1.GetAMU() << '\n';
@@ -186,23 +217,32 @@ void Simulation_S2029(const std::string& beam = "17F", double T1 = 4.5, double E
     //plotting
     auto* c0 {new TCanvas("c0", "Canvas for Energy Loss Plots")};
 
-    hT1Initial->SetLineColor(kBlue);
+    hT1Initial->SetLineColor(kMagenta);
     hT1Initial->SetStats(0);
     auto* temp1 = hT1Initial->DrawClone(); // keep clone in temp to reuse in the legend
     hT1AfterIC->SetLineColor(kRed);
     auto* temp2 = hT1AfterIC->DrawClone("same");
+    hT1AfterCFA->SetLineColor(kBlue);
+    auto* temp3 = hT1AfterCFA->DrawClone("same");
 
     auto* l0 = new TLegend(0.1,0.7,0.5,0.9); 
     l0->AddEntry(temp1, "Initial Beam Energy","l");
-    l0->AddEntry(temp2, "EBeam after IC","l");
+    l0->AddEntry(temp2, TString::Format("EBeam after IC @ %.0fmbar",pressureIC),"l");
+    l0->AddEntry(temp3, TString::Format("EBeam after CFA @ %.0fmbar",pressureCFA),"l");
 
     if (trackContamination){
-        hTCInitial->SetLineColor(kOrange);
-        auto* temp3 = hTCInitial->DrawClone("same");
-        hTCAfterIC->SetLineColor(kMagenta);
-        auto* temp4 = hTCAfterIC->DrawClone("same");
-        l0->AddEntry(temp3, "17O Initial Energy","l");
-        l0->AddEntry(temp4, "17O after IC","l");
+        hTCInitial->SetLineColor(kMagenta);
+        hTCInitial->SetLineStyle(2);
+        auto* temp4 = hTCInitial->DrawClone("same");
+        hTCAfterIC->SetLineColor(kRed);
+        hTCAfterIC->SetLineStyle(2);
+        auto* temp5 = hTCAfterIC->DrawClone("same");
+        hTCAfterCFA->SetLineColor(kBlue);
+        hTCAfterCFA->SetLineStyle(2);
+        auto* temp6 = hTCAfterCFA->DrawClone("same");
+        l0->AddEntry(temp4, TString::Format("%s Initial Energy",p1c.GetName().c_str()),"l");
+        l0->AddEntry(temp5, TString::Format("%s after IC @ %.0fmbar",p1c.GetName().c_str(),pressureIC),"l");
+        l0->AddEntry(temp6, TString::Format("%s after CFA @ %.0fmbar",p1c.GetName().c_str(),pressureCFA),"l");
     }
 
     l0->Draw();
