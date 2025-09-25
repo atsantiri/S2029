58c6ee4b69ee4afdbf0a6e9f6df195f254b9d9ce
atsantiri <artemis@tsantiri@hotmail.com>
2025-09-25 16:09:56 -0600

    finished dE comparison for runs with pad polarization, and verified 17O vs 17F beam

--- Macros/beamdE.cxx
+++ Macros/beamdE.cxx
@@ -1,9 +1,9 @@
 #include "ActDataManager.h"
+#include "ActMergerData.h"
 #include "ActModularData.h"
 #include "ActParticle.h"
 #include "ActSRIM.h"
 #include "ActTPCData.h"
-
 #include "ActTypes.h"
 
 #include "ROOT/RDataFrame.hxx"
@@ -11,25 +11,31 @@
 #include "TCanvas.h"
 #include "TH2.h"
 #include "TLegend.h"
+#include "TLine.h"
 #include "TROOT.h"
 
 #include <fstream>
 
 
-void make_dE(int run, TCanvas* c, TLegend* l, int pad)
+TH1D* make_dE(int run, TCanvas* c, TLegend* l, int pad)
 {
-    ActRoot::DataManager dataman {"../configs/data.conf", ActRoot::ModeType::EFilter};
+    ActRoot::DataManager dataman {"../configs/data.conf", ActRoot::ModeType::EMerge};
     dataman.SetRuns(run, run);
     auto chain {dataman.GetChain()};
-    auto chain2 {dataman.GetChain(ActRoot::ModeType::EReadSilMod)};
+    auto chain2 {dataman.GetChain(ActRoot::ModeType::EFilter)};
+    auto chain3 {dataman.GetChain(ActRoot::ModeType::EReadSilMod)};
     chain->AddFriend(chain2.get());
+    chain->AddFriend(chain3.get());
 
     ROOT::RDataFrame df {*chain};
 
 
-    auto gated {df.Filter([](ActRoot::TPCData& tpc, ActRoot::ModularData& mod)
-                          { return (mod.Get("GATCONF") == 64) && (tpc.fClusters.size() == 1); },
-                          {"TPCData", "ModularData"})
+    auto gated {df.Filter(
+                      [](ActRoot::TPCData& tpc, ActRoot::ModularData& mod, ActRoot::MergerData& mer)
+                      {
+                          return (mod.Get("GATCONF") == 64) && (tpc.fClusters.size() == 1) && (mer.fEntry > 500) && (mer.fEntry < 1354);
+                      },
+                      {"TPCData", "ModularData", "MergerData"})
                     .Define("GATCONF", [](ActRoot::ModularData& mod)
                             { return static_cast<int>(mod.fLeaves["GATCONF"]); }, {"ModularData"})};
 
@@ -37,15 +43,15 @@ void make_dE(int run, TCanvas* c, TLegend* l, int pad)
     gated.Foreach(
         [&](const int& gatconf)
         {
-            if(gatconf == 64)
-                cfa++;
+            // if(gatconf == 64)
+            cfa++;
         },
         {"GATCONF"});
 
 
     auto h2d = new TH2D {
         TString::Format("hPad%d", run), TString::Format("Pad plane %d;X [pad];Y [pad]", run), 128, 0, 128, 128, 0, 128};
-    auto hdE = new TH1D {TString::Format("hdE%d", run), "; X [pad]; Q per pad", 50, 0, 50};
+    auto hdE = new TH1D {TString::Format("hdE%d", run), "; X [pad]; Q per pad", 30, 0, 30};
     gated.Foreach(
         [&](ActRoot::TPCData& tpc)
         {
@@ -73,9 +79,10 @@ void make_dE(int run, TCanvas* c, TLegend* l, int pad)
     c->cd(3);
     hdE->DrawClone("same");
 
+    return hdE;
 }
 
-void do_simu(const std::string p, TCanvas* c, TLegend* l, int color)
+TH1D* do_simu(const std::string p, TCanvas* c, TLegend* l, int color)
 {
     auto* srim {new ActPhysics::SRIM()};
     ActPhysics::Particle beam {p};
@@ -102,7 +109,7 @@ void do_simu(const std::string p, TCanvas* c, TLegend* l, int color)
     for(const auto& [label, path] : tables)
     {
         if(!load_table(label, path))
-            return;
+            return nullptr;
     }
 
     int iter {1000};
@@ -124,9 +131,11 @@ void do_simu(const std::string p, TCanvas* c, TLegend* l, int color)
         }
     }
     hSim->SetLineColor(color);
-    hSim->Scale(1./iter);
+    hSim->Scale(1. / iter);
     hSim->Draw("same");
     l->AddEntry(hSim, p.c_str());
+
+    return hSim;
 }
 
 
@@ -136,25 +145,66 @@ void beamdE()
     bool compare_with_simu {false};
     // ROOT::EnableImplicitMT();
     auto* c {new TCanvas {"c", "Data", 1500, 800}};
-    auto l = new TLegend(0.65, 0.75, 0.9, 0.9);
+    auto l = new TLegend(0.5, 0.1, 0.9, 0.3);
 
     c->Divide(3);
-    auto htemp = new TH2D {"htemp", "; X [pad]; Q per pad", 100, 0, 30, 100, 0, 7000};
+    auto htemp = new TH2D {"htemp", "; X [pad]; Q per pad", 100, 0, 30, 100, 500, 7000};
     htemp->SetStats(false);
     c->cd(3);
     htemp->Draw();
 
-    make_dE(43, c, l, 1); // 43 is 50V polarization before changing charged state
-    make_dE(45, c, l, 2); // 45 is with 9+ charged state at 70V polarization
+    auto hO = make_dE(43, c, l, 1); // 43 is 50V polarization before changing charged state
+    auto hF = make_dE(45, c, l, 2); // 45 is with 9+ charged state at 70V polarization
     l->Draw();
 
     // How does the simulation with srim compare for the two?
     auto* c1 {new TCanvas {"c1", "Simu", 1000, 800}};
+    c1->Divide(2);
+    c1->cd(1);
     auto htemp2 = new TH2D {"htemp2", "; X [mm]; dE/dx", 100, 0, 60, 100, 0, .5};
     auto l1 = new TLegend(0.7, 0.8, 0.9, 0.9);
     htemp2->SetStats(false);
     htemp2->Draw();
-    do_simu("17O", c1, l1, 1);
-    do_simu("17F", c1, l1, 2);
+    auto hsimO = do_simu("17O", c1, l1, 1);
+    auto hsimF = do_simu("17F", c1, l1, 2);
     l1->Draw();
+
+
+    // Find what the ratio b/w 17O and 17F should have been
+    TH1D* hRatioSim = (TH1D*)hsimF->Clone("hRatioSim");
+    hRatioSim->Divide(hsimO);
+    c1->cd(2);
+    hRatioSim->GetXaxis()->SetLimits(0, 30);
+    hRatioSim->Draw();
+
+    c->cd(3);
+    TH1D* hOscaled = (TH1D*)hO->Clone("hOscaled");
+    hOscaled->Multiply(hRatioSim);
+    hOscaled->SetLineColor(3);
+    hOscaled->Draw("same");
+    l->AddEntry(hOscaled, "where F should have been");
+    l->Draw();
+
+    // Now what is the ratio of the corrected 17O tp 17F. Is it the 70V/50V of the different polarization in the two
+    // runs?
+    TH1D* hRatio2 = (TH1D*)hOscaled->Clone("hRatio2");
+    hRatio2->Divide(hF);
+
+    auto* c2 {new TCanvas {"c2", "", 800, 800}};
+    c2->cd();
+    hRatio2->SetLineColor(1);
+    hRatio2->GetYaxis()->SetRangeUser(0, 2);
+    hRatio2->Draw();
+    // TH1D* hRatio2Corrected = (TH1D*)hRatio2->Clone("hRatio2Corrected");
+    // hRatio2Corrected->Divide(hRatioSim);
+    // hRatio2Corrected->SetLineColor(2);
+    // hRatio2Corrected->Draw("same");
+
+    auto l2 = new TLegend(0.7, 0.8, 0.9, 0.9);
+    l2->AddEntry(hRatio2, "7/5?");
+    TLine* line = new TLine(0, 7. / 5., 30, 7. / 5.);
+    line->SetLineColor(kRed);
+    line->Draw("same");
+    l2->AddEntry(line, "7/5");
+    l2->Draw();
 }
--- Macros/compKin.cxx
+++ Macros/compKin.cxx
@@ -2,21 +2,27 @@
 
 #include "TCanvas.h"
 #include "TGraph.h"
+#include "TLegend.h"
 #include "TMultiGraph.h"
+
 void compKin()
 {
-    ActPhysics::Kinematics kf {"17F(p,p)@50"};
-    ActPhysics::Kinematics ko {"17O(p,p)@65"};
+    ActPhysics::Kinematics kp {"17F(p,p)@50"};
+    ActPhysics::Kinematics ka {"17F(p,a)@50"};
 
     auto* mg {new TMultiGraph};
     mg->SetTitle("Kin comparison;#theta_{Lab} [#circ];E_{Lab} [MeV]");
-    auto* gf {kf.GetKinematicLine3()};
-    gf->SetTitle("17F");
-    auto* go {ko.GetKinematicLine3()};
-    go->SetTitle("17O");
-    mg->Add(gf);
-    mg->Add(go);
+    auto* gp {kp.GetKinematicLine3()};
+    gp->SetTitle("17F(p,p)");
+    auto* ga {ka.GetKinematicLine3()};
+    ga->SetTitle("17F(p,a)");
+    mg->Add(gp);
+    mg->Add(ga);
 
     auto* c0 {new TCanvas {"c0", "Comp kin"}};
     mg->Draw("apl plc pmc");
+    auto l = new TLegend(0.7, 0.7, 0.9, 0.9);
+    l->AddEntry(gp, gp->GetTitle());
+    l->AddEntry(ga, ga->GetTitle());
+    l->Draw();
 }
